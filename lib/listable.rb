86255e4e3bd359f9cebb57ffda628965e83f56ca
Jacob Howarth <jshowa.git@gmail.com>
2016-02-13 18:18:54 -0500

    Added format description, date, priority to Listable module and included in Link, Todo, Event Item classes.

--- lib/listable.rb
+++ lib/listable.rb
@@ -1,3 +1,22 @@
+# coding: utf-8
 module Listable
   # Listable methods go here
+  def format_description(description)
+    "#{description}".ljust(25)
+  end
+
+  def format_date(date_list = {})
+    dates = date_list[:start_date] ? date_list[:start_date].strftime("%D") : "No due date"
+    dates << " -- " + date_list[:end_date].strftime("%D") if date_list[:end_date]
+    dates = "N/A" if date_list.empty?
+    return dates
+  end
+
+  def format_priority(priority)
+    value = " ⇧" if priority == "high"
+    value = " ⇨" if priority == "medium"
+    value = " ⇩" if priority == "low"
+    value = "" if !priority
+    return value
+  end
 end
